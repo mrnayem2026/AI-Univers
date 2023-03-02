d86400d59790b1dd44085ca608aac4ff0e3e32c4
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-03-02 22:55:59 +0600

    Show Ai All Tools from Api when click show all date button

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ const loadAiDate = async() => {
     const res = await fetch(URL)
     const date = await res.json()
     showAiData(date.data.tools);
+    showBtn(date.data.tools);
 }
 
+// Show Ai tools Card upto 6 start
 const showAiData = (data) =>{
-    console.log(data[0].published_in);
    data = data.splice(0,6)
     data.forEach( data => {
        document.getElementById('aiCardContainer').innerHTML  += `   
@@ -37,5 +38,47 @@ const showAiData = (data) =>{
 
     })
 }
+// Show Ai tools Card upto 6 end
+
+// Show Ai All Tools from Api [start] 
+const showBtn = (data) =>{
+    let showAllDateContainer = document.getElementById('showAllDateContainer');
+    if(data.length > 0 ) 
+    {
+        showAllDateContainer.classList.remove('hidden');
+    }
+    showAllDateContainer.addEventListener('click',function(){
+        data.forEach( data => {
+            document.getElementById('aiCardContainer').innerHTML  += `   
+            <div class="card w-96 bg-base-100 shadow-xl border">
+            <figure class="px-10 pt-10">
+              <img src="${data.image}" alt="Shoes" class="Ai Tools" />
+            </figure>
+            <div class="card-body">
+              <h2 class="text-3xl  font-black">Features</h2>
+              <p class="font-normal text-sm">1. <span>${data.features[0]}</span></p>
+              <p class="font-normal text-sm">2. <span>${data.features[1]}</span></p>
+              <p class="font-normal text-sm">3. <span>${data.features[2]}</span></p>
+              <hr class="border">
+           
+             <section class="flex justify-between">
+                <div>
+                    <h1 id="cardTitle" class="text-3xl  font-black">${data.name}</h1> <br>
+                    <p><img src="./icon/icon.png" alt="Date Icon" class="inline" > ${data.published_in} </p>
+                </div>
+                <div class="card-actions pt-5">
+                    <button class="btn btn-outline btn-error text-sm font-bold">Details</button>
+                  </div>
+             </section>
+              
+            </div>
+          </div>       
+            `
+     
+         })
+         showAllDateContainer.classList.add('hidden');
+    })
+}
+// Show Ai All Tools from Api [End] 
 
 loadAiDate()
